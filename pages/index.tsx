a3b2f01c07bdd23434c20f29a7413e1675cea888
Andrii Shupta <andrii.shupta@gmail.com>
2022-12-12 19:15:18 +0200

    feat: add github link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 
 
 const Connect = dynamic(() => import('../components/Connect').then(m => m.Connect), {
@@ -12,6 +13,14 @@ export default function Home() {
       <Head>
         <title>@polkadot/extension-dapp example</title>
       </Head>
+      <a
+        className="absolute top-4 right-4"
+        href="https://github.com/andriishupta/polkadot-extension-dapp-example"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image src="/github.png" alt="andriishupta's github" width={32} height={32}/>
+      </a>
       <main>
         <div className="px-6 lg:px-8">
           <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
